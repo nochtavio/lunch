0af1c8b647f44adf5a08993ff9eaa77f209697a6
nochtavio <anochtavio@gmail.com>
2020-04-19 11:28:54 +0700

    add run script to testing

--- run.sh
+++ run.sh
@@ -8,6 +8,18 @@ case $operator in
     "default")
         docker-compose up --build
         ;;
+    "test")
+        docker-compose down
+        docker-compose up -d
+        docker-compose exec -e -T app sh -c "php bin/phpunit"
+        if [ $? != 0 ]; then
+            printf "\nTests failed!\n\n"
+            EXIT_CODE=1
+        else
+            printf "\nTests passed\n\n"
+        fi
+        docker-compose down
+        ;;
     *)
         printf "Unidentified Action :( !! \n\n"
         EXIT_CODE=1
